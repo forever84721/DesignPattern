a01106804f6b6691d0430775088140a183d43559
Jay <forever84721@yahoo.com.tw>
2019-01-12 21:47:55 +0800

    Singleton Pattern

---
+++ DesignPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SingletonPattern
+{
+    public class Singleton//餓漢式 ??
+    {
+        private Singleton() { }
+        private static Singleton instance = new Singleton();
+        public static Singleton GetSingleton()
+        {
+            return instance;
+        }
+        public void SingletonTest()
+        {
+            Console.WriteLine("=== Singleton Test Text ===");
+        }
+    }
+
+}
---
+++ DesignPattern/SingletonPattern/Singleton2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SingletonPattern
+{
+    public class Singleton2//登記式/內部靜態class
+    {
+        private static class Singleton2Holder
+        {
+            public static readonly Singleton2 singleton = new Singleton2();
+        }
+        private Singleton2() { }
+        public static Singleton2 GetSingleton()
+        {
+            return Singleton2Holder.singleton;
+        }
+        public void SingletonTest()
+        {
+            Console.WriteLine("=== Singleton2 Test Text ===");
+        }
+    }
+}
